0c514b0fbd7de5fd8fd1a8c2e6ce5f8496299c36
fanxingqiang <lzhdfxq@163.com>
2018-07-22 17:51:54 +0800

    修改

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,49 @@
 import { Component } from '@angular/core';
 import * as Handsontable from 'handsontable-pro';
+import {HotTableRegisterer} from '@handsontable-pro/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HotTableRegisterer]
 })
 export class AppComponent {
-  dataset: any[] = [
-    {id: 1, name: 'Ted Right', address: 'Wall Street'},
-    {id: 2, name: 'Frank Honest', address: 'Pennsylvania Avenue'},
-    {id: 3, name: 'Joan Well', address: 'Broadway'},
-    {id: 4, name: 'Gail Polite', address: 'Bourbon Street'},
-    {id: 5, name: 'Michael Fair', address: 'Lombard Street'},
-    {id: 6, name: 'Mia Fair', address: 'Rodeo Drive'},
-    {id: 7, name: 'Cora Fair', address: 'Sunset Boulevard'},
-    {id: 8, name: 'Jack Right', address: 'Michigan Avenue'},
-  ];
+  instance: string = 'hot';
+  dataset: any[] = Handsontable.helper.createSpreadsheetData(10, 10);
+
+  constructor(private hotRegisterer: HotTableRegisterer) {}
+
+  clearSelection() {
+    const hotInstance = this.hotRegisterer.getInstance(this.instance);
+
+    if (hotInstance) {
+      hotInstance.emptySelectedCells();
+      hotInstance.setDataAtCell(0, 0, 'new value');
+    }
+  }
+
+  exportFile() {
+    const hotInstance = this.hotRegisterer.getInstance(this.instance);
+    const exportPlugin = hotInstance.getPlugin('exportFile');
+
+    // export as a string
+    exportPlugin.exportAsString('csv');
+
+    // export as a blob object
+    exportPlugin.exportAsBlob('csv');
+
+    // export to downloadable file (named: MyFile.csv)
+    exportPlugin.downloadFile('csv', {filename: 'MyFile'});
+
+    // export as a string (with specified data range):
+    exportPlugin.exportAsString('csv', {
+      exportHiddenRows: true,     // default false
+      exportHiddenColumns: true,  // default false
+      columnHeaders: true,        // default false
+      rowHeaders: true,           // default false
+      columnDelimiter: ';',       // default ','
+      range: [1, 1, 6, 6]         // [startRow, endRow, startColumn, endColumn]
+    });
+  }
 }
